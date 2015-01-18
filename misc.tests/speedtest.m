53170f39b996c8c1145050804cecc92fb31caadb
williamandregg <wandregg@gmail.com>
2015-01-18 10:15:36 -0800

    small changes
    Signed-off-by: williamandregg <wandregg@gmail.com>

--- misc.tests/speedtest.m
+++ misc.tests/speedtest.m
@@ -3,10 +3,14 @@
 %so that the time to create the random matrices is not included in the
 %calculation
 clear all;
-load('randmats');
+%load('randmats');
+tic
+a=rand(1000);
+b=rand(1000);
+toc
 a=.001*a;
 b=.001*b;
 tic
 c=a*b;
 toc
-d=ones(1,length(c))*c*ones(length(c),1)
+d=ones(1,length(c))*c*ones(length(c),1) %#ok<NOPTS>
